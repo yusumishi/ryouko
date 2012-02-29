a75c8d2af7b51fb44bf8559ada6b6309afea09e2
Daniel Sim <foxhead128@Mishiko.(none)>
2012-02-28 19:03:37 -0500

    Bumped to version 0.4.3

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from distutils.core import setup
 
 setup(
     name = 'ryouko',
-    version = '0.4.2',
+    version = '0.4.3',
     description = 'PyQt4 Web browser',
-    long_description = """Ryouko is a basic PyQt4 Web browser.""",
+    long_description = """Ryouko is a basic PyQt4 Web browser. It was coded for fun and is not intended for serious usage, but it should be capable of fulfilling very basic browsing needs.""",
     scripts = ['ryouko'],
     packages = ['ryouko_lib'],
     package_data = {'ryouko_lib' : files },
     author = 'Daniel Sim',
     author_email =  'foxhead128@gmail.com',
-    url = "http://sourceforge.net/projects/fh-miscellany/",
+    url = "http://sourceforge.net/projects/ryouko/",
     license = "MIT"
 )
