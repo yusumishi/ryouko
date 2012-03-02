47e2284a3ad466b3f819275fecc409228512112e
unknown <DS_2@Kiyoko.(none)>
2012-03-01 21:24:59 -0500

    Bumped to 0.5.4.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name = 'ryouko',
-    version = '0.5.3',
+    version = '0.5.4',
     description = 'PyQt4 Web browser',
     long_description = """Ryouko is a basic PyQt4 Web browser. It was coded for fun and is not intended for serious usage, but it should be capable of fulfilling very basic browsing needs.""",
     scripts = ['ryouko'],
