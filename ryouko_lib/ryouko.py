df0bd0ee9f294183b1748aeb96c97285744e43e5
Daniel Sim <foxhead128@Sayo.(none)>
2012-07-01 06:04:06 -0400

    Set search editor to center itself if not a menu.

--- ryouko_lib/ryouko.py
+++ ryouko_lib/ryouko.py
@@ -315,24 +315,31 @@ class RMenuPopupWindow(QtGui.QMainWindow):
         return self.mainLayout
     def primeDisplay(self):
         return
+    def center(self):
+        fg = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        fg.moveCenter(cp)
+        self.move(fg.topLeft())
     def display(self, menu = False, x = 0, y = 0, width = 0, height = 0):
         self.primeDisplay()
         if menu == True:
             self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Popup)
             self.setStyleSheet(self.styleSheet)
+            self.show()
+            if x - self.width() + width < 0:
+                x = 0
+            else:
+                x = x - self.width() + width
+            if y + height + self.height() >= QtGui.QApplication.desktop().size().height():
+                y = y - self.height()
+            else:
+                y = y + height
+            self.move(x, y)
         else:
             self.setWindowFlags(QtCore.Qt.Widget)
             self.setStyleSheet("")
-        self.show()
-        if x - self.width() + width < 0:
-            x = 0
-        else:
-            x = x - self.width() + width
-        if y + height + self.height() >= QtGui.QApplication.desktop().size().height():
-            y = y - self.height()
-        else:
-            y = y + height
-        self.move(x, y)
+            self.show()
+            self.center()
 
 class SearchEditor(RMenuPopupWindow):
     def __init__(self, parent=None):
