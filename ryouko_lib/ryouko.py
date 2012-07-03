5d5dc7fa8ef251916f545806a8bfba37536f046e
Daniel Sim <foxhead128@Mishiko.(none)>
2012-07-03 17:11:48 -0400

    Split off RMenuPopupWindow and renamed it to MenuPopupWindow

--- ryouko_lib/ryouko.py
+++ ryouko_lib/ryouko.py
@@ -54,6 +54,7 @@ from SettingsManager import *
 from DownloaderThread import *
 from DialogFunctions import *
 from RTabWidget import *
+from MenuPopupWindow import *
 from ViewSourceDialog import *
 from RExpander import *
 from SearchManager import *
@@ -256,48 +257,7 @@ def touch(fname):
 
 searchManager = SearchManager(app_profile)
 
-class RMenuPopupWindow(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        super(RMenuPopupWindow, self).__init__(parent)
-        self.parent = parent
-        self.widget = QtGui.QWidget(self)
-        self.setCentralWidget(self.widget)
-        self.mainLayout = QtGui.QVBoxLayout()
-        self.mainLayout.setContentsMargins(0,0,0,0)
-        self.mainLayout.setSpacing(0)
-        self.styleSheet = "QMainWindow { border: 1px solid palette(shadow);} QToolButton:focus, QPushButton:focus { background: palette(highlight); border: 1px solid palette(highlight); color: palette(highlighted-text); }"
-        self.widget.setLayout(self.mainLayout)
-    def layout(self):
-        return self.mainLayout
-    def primeDisplay(self):
-        return
-    def center(self):
-        fg = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        fg.moveCenter(cp)
-        self.move(fg.topLeft())
-    def display(self, menu = False, x = 0, y = 0, width = 0, height = 0):
-        self.primeDisplay()
-        if menu == True:
-            self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Popup)
-            self.setStyleSheet(self.styleSheet)
-            self.show()
-            if x - self.width() + width < 0:
-                x = 0
-            else:
-                x = x - self.width() + width
-            if y + height + self.height() >= QtGui.QApplication.desktop().size().height():
-                y = y - self.height()
-            else:
-                y = y + height
-            self.move(x, y)
-        else:
-            self.setWindowFlags(QtCore.Qt.Widget)
-            self.setStyleSheet("")
-            self.show()
-            self.center()
-
-class SearchEditor(RMenuPopupWindow):
+class SearchEditor(MenuPopupWindow):
     def __init__(self, parent=None):
         super(SearchEditor, self).__init__(parent)
         self.parent = parent
