6b75efb314ffe4265a9d475a7998a57d3c33d7bb
Daniel Sim <foxhead128@Mishiko.(none)>
2012-07-05 17:22:43 -0400

    Updated default user agent. Ryouko now identifies itself as Ryouko! Yay!

--- ryouko_lib/ryouko.py
+++ ryouko_lib/ryouko.py
@@ -3055,7 +3055,7 @@ class Ryouko(QtGui.QWidget):
         dA.setHtml("<html><body><span id='userAgent'></span></body></html>")
         dA.page().mainFrame().evaluateJavaScript("document.getElementById(\"userAgent\").innerHTML = navigator.userAgent;")
         global app_default_useragent
-        app_default_useragent = unicode(dA.page().mainFrame().findFirstElement("#userAgent").toPlainText())
+        app_default_useragent = unicode(dA.page().mainFrame().findFirstElement("#userAgent").toPlainText()).replace("Safari", "Ryouko/" + app_version + " Safari")
         print(app_default_useragent)
         dA.deleteLater()
         if not os.path.isdir(app_profile):
