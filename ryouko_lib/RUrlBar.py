dff9928983b7d8d25804835a2c07d1e687b663e4
Daniel Sim <foxhead128@Mishiko.(none)>
2012-07-21 02:30:21 -0400

    Redefined RUrlBar's paintEvent so it defines the widget's stylesheet using the height obtained after rendering. Then the event redefines itself to reduce future computation efforts.

--- ryouko_lib/RUrlBar.py
+++ ryouko_lib/RUrlBar.py
@@ -20,12 +20,26 @@ class RUrlBar(QtGui.QLineEdit):
         self.icon.setFixedWidth(16)
         self.icon.setFixedHeight(16)
         self.icon.setStyleSheet("QToolButton { border: 0; background: transparent; width: 16px; height: 16px; }")
+        sz = self.icon
         fw = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
-        self.setStyleSheet("QLineEdit { padding-left: %spx; }" % str(self.icon.sizeHint().width() + fw + 1))
+        self.s = False
         msz = self.minimumSizeHint()
         self.setMinimumSize(qMax(msz.width(), self.icon.sizeHint().height() + fw * 2 + 2), qMax(msz.height(), self.icon.sizeHint().height() + fw * 2 + 2))
 
     def paintEvent(self, ev):
+        sz = self.icon
+        fw = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
+        QtGui.QLineEdit.paintEvent(self, ev)
+        self.icon.render(self, QtCore.QPoint(self.rect().left() + (self.height() + 1 - sz.width())/2, (self.height() + 1 - sz.height())/2))
+        if self.s == False:
+            self.setStyleSheet("QLineEdit { padding-left: %spx; }" % str(sz.width() + (self.height() + 1 - sz.width())/2))
+            self.s = True
+            self.redefPaintEvent()
+
+    def redefPaintEvent(self):
+        self.paintEvent = self.shortPaintEvent
+
+    def shortPaintEvent(self, ev):
         sz = self.icon
         fw = self.style().pixelMetric(QtGui.QStyle.PM_DefaultFrameWidth)
         QtGui.QLineEdit.paintEvent(self, ev)
