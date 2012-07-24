0d9439b95665e35f2bf7d9b2242257490b12685a
unknown <DS_2@Kiyoko.(none)>
2012-07-24 15:59:36 -0400

    Fully removed theme integration.

--- ryouko_lib/MovableTabWidget.py
+++ ryouko_lib/MovableTabWidget.py
@@ -18,61 +18,16 @@ from MovableTabBar import *
 def doNothing():
     return
 
-win_stylesheet = """
-QTabBar::tab {
-padding: 4px;
-border: 1px solid palette(shadow);
-}
-
-QTabBar::tab:top {
-border-top-left-radius: 4px;
-border-top-right-radius:4px;
-border-bottom: 1px solid palette(shadow);
-background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop:0 palette(window), stop:1 palette(midlight));
-}
-
-QTabBar::tab:top:selected {
-border-bottom: 0;
-padding-bottom: 5px;
-background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop:0 palette(light), stop:1 palette(window));
-}
-"""
+win_stylesheet = ""
 
 class MovableTabWidget(QtGui.QTabWidget):
     def __init__(self, parent=None, forcea=False):
         super(MovableTabWidget, self).__init__(parent)
 
-        self.gnome_session = False
-        if sys.platform.startswith("linux"):
-            output = commands.getoutput('ps -A')
-            if 'gnome-session' in output:
-                self.gnome_session = True
-
         self.parent = parent
         self.nuTabBar = MovableTabBar(self.parent)
         self.setTabBar(self.nuTabBar)
         self.setDocumentMode(True)
-
-        #theme = get_key("/desktop/gnome/shell/windows/theme")
-
-        """"if theme == "Ambiance" and self.gnome_session:
-            self.setStyleSheet(win_stylesheet)
-            self.parent.setStyleSheet("""
-            #QMainWindow {
-            #background: #3C3B37;
-            #}
-        """)
-
-        elif theme == "Radiance" and self.gnome_session:
-            self.setStyleSheet(win_stylesheet)
-            self.parent.setStyleSheet("""
-            #QMainWindow {
-            #background: #DFD7CF;
-            #}
-        """)"""
-
-        if sys.platform.startswith("win"):
-            self.setStyleSheet(win_stylesheet)
 
         self.mouseX = False
         self.mouseY = False
