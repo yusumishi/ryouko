23049638930f2ef3fb4579255592f30ab9cc3c3c
Daniel Sim <foxhead128@Mishiko.(none)>
2012-08-07 09:03:30 -0400

    Fixed some bugs.

--- ryouko_lib/ryouko.py
+++ ryouko_lib/ryouko.py
@@ -1100,10 +1100,8 @@ class RAboutDialog(QtGui.QMainWindow):
         self.licensePage.setPage(page2)
         self.licensePage.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         h = os.path.join(app_lib, "LICENSE.html").replace("\\", "/")
-        print(h[0:3])
         if sys.platform.startswith("win"):
-            h = h.replace(h[0:3], "")
-        print(h)
+            h = h.replace(h[0:2], "")
         self.licensePage.load(QtCore.QUrl(h))
 
         self.tabs.addTab(self.licensePage, tr("licenseHKey"))
