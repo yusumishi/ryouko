925694b7dc9f85ce685aa4981c69dd688ba86138
Daniel Sim <foxhead128@Mishiko.(none)>
2012-08-21 16:16:32 -0400

    Updated serverthread to run only on localhost.

--- ryouko_lib/ServerThread.py
+++ ryouko_lib/ServerThread.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-import os.path, sys
+import os.path
 from os import chdir
 from PyQt4 import QtCore
-from SimpleHTTPServer import test
-app_lib = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(app_lib)
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import BaseHTTPServer
 
 class ServerThread(QtCore.QThread):
     def __init__(self, parent=None):
@@ -16,6 +15,18 @@ class ServerThread(QtCore.QThread):
     def setDirectory(self, directory):
         self.directory = directory
     def run(self):
-        if os.path.exists(self.directory):
+        if os.path.isdir(self.directory):
             chdir(self.directory)
-            test()
+            HandlerClass = SimpleHTTPRequestHandler
+            ServerClass  = BaseHTTPServer.HTTPServer
+            Protocol     = "HTTP/1.0"
+
+            port = 8000
+            server_address = ('127.0.0.1', port)
+
+            HandlerClass.protocol_version = Protocol
+            httpd = ServerClass(server_address, HandlerClass)
+
+            sa = httpd.socket.getsockname()
+            print("Serving HTTP on", sa[0], "port", sa[1], "...")
+            httpd.serve_forever()
